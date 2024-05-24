668c1956030cc6d338dddf6c0d9a576c7dc73bbe
JuanPabloG16 <93049031+JuanPabloG16@users.noreply.github.com>
2024-05-24 01:04:18 -0500

    fin3

--- expressJs/app.js
+++ expressJs/app.js
@@ -48,7 +48,7 @@ app.get('/personas', (req, res) => {
 });
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || servicionube-production.up.railway.app;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
